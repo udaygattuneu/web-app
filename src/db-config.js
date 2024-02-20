7b56915ae735382e99d296c042fad34b54d1cf8e
udaygattuneu <gattu.u@northeastern.edu>
2024-02-20 13:43:40 -0500

    rrr

--- src/db-config.js
+++ src/db-config.js
@@ -3,7 +3,7 @@ module.exports = {
     USER: `${process.env.DB_USER}`,
     PASSWORD: `${process.env.DB_PASSWORD}`,
     DB: `${process.env.DB_NAME}`,
-    dialect:  `${process.env.DB_DIALECT}`,
+    dialect:  'mysql',
     pool: {
       max: 5,
       min: 0,
