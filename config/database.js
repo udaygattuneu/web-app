042c583f725de1b61457052696be28fd24cc39a5
udaygattuneu <145166875+udaygattuneu@users.noreply.github.com>
2024-02-27 09:39:59 -0500

    build testing

--- config/database.js
+++ config/database.js
@@ -1,24 +1,38 @@
 import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
-import { fileURLToPath } from 'url';
-import path from 'path';
+// import { fileURLToPath } from 'url';
+// import path from 'path';
 
 dotenv.config();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+console.log("DB Name:", process.env.DB_NAME);
+console.log("DB User:", process.env.DB_USER);
+console.log("DB Password:", process.env.DB_PASSWORD);
+console.log("DB Host:", process.env.DB_HOST);
 
 
-const mypath = path.join(__dirname, '../.env');
+// const mypath = path.join(__dirname, './.env');
+// const sequelize = new Sequelize(
+    
+//     "db1", 
+//   "root", 
+//     "Uday123@", 
+//     {
+//     host: "localhost",
+//     dialect: 'mysql',
+//     logging: false 
+// });
 const sequelize = new Sequelize(
     
-    process.env.DB_NAME, 
-  process.env.DB_USER, 
-    process.env.DB_PASSWORD, 
-    {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false 
+  process.env.DB_NAME, 
+process.env.DB_USER, 
+  process.env.DB_PASSWORD, 
+  {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  logging: false 
 });
-//
+
 export default sequelize;
 
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-import express from 'express';
+
+
 import sequelize from './config/database.js'; 
+import express from 'express';
 import router from './routes/router.js'; 
 
 const app = express();
@@ -16,38 +18,16 @@ app.use(router);
 
 
 
-// sequelize.sync().then(() => {
-//   console.log('Database bootstrapped successfully');
-//   app.listen(5002, () => {
-//     console.log(`Server is running on port 5002}`);
-//   });
-// }).catch((error) => {
-//   console.error('Unable to sync the database:', error);
-// });
-
-let retries = 3;
-const delay = 5000; // Delay in milliseconds
-
-function syncDatabase(retriesLeft = retries) {
-  sequelize.sync().then(() => {
-    console.log('Database schema synchronized successfully.');
-    app.listen(5002, () => {
-      console.log(`Server is running on port 5002}`);
-    });
-  }).catch((error) => {
-    console.error('Unable to sync the database:', error);
-    if (retriesLeft > 0) {
-      console.log(`Retrying... (${retriesLeft} attempts left)`);
-      setTimeout(() => syncDatabase(retriesLeft - 1), delay);
-    } else {
-      console.error('All retry attempts failed. Exiting the application...');
-
-      process.exit(1);
-    }
+sequelize.sync({force:true}).then(() => {
+  console.log('Database bootstrapped successfully');
+  app.listen(5002, () => {
+    console.log(`Server is running on port 5002}`);
   });
-}
+}).catch((error) => {
+  console.error('Unable to sync the database:', error);
+});
 
-syncDatabase();
+
 
 
 
