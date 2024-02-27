67a8a655bc03a88400f564284757a03af7e42827
udaygattuneu <145166875+udaygattuneu@users.noreply.github.com>
2024-02-26 19:16:41 -0500

    Update router.js (#32)

--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,21 @@
+
+
 import express from 'express';
+
+import { healthCheckMiddleware, methodNotAllowed } from '../middlewares/healthCheck.js';
+import { healthCheck, createUser, getUser, updateUser } from '../controllers/controllers.js';
+
 const router = express.Router();
-import {healthCheckMiddleware, methodNotAllowed} from '../middlewares/healthCheck.js';
-import { healthCheck, createUser, getUser, updateUser } from '../controllers/controllers.js'; 
 
 
-router.get('/healthz', healthCheckMiddleware, healthCheck);
-router.all('/healthz', healthCheckMiddleware, methodNotAllowed); 
-router.post('/v1/user', createUser);
-router.get('/v1/user/self', getUser);
-router.put('/v1/user/self', updateUser);
+router.route('/healthz')
+  .get(healthCheckMiddleware, healthCheck) 
+  .all(healthCheckMiddleware, methodNotAllowed); 
+
+
+router.post('/v1/user', createUser); 
+router.route('/v1/user/self')
+  .get(getUser) 
+  .put(updateUser); 
 
 export default router;
