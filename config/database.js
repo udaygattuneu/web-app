6991019cb14ccaf09df1898ad720b8d2e1a0c804
udaygattuneu <145166875+udaygattuneu@users.noreply.github.com>
2024-02-27 09:44:37 -0500

    Packer adding v5 (#49)
    * correcting the errors
    
    * correcting
    
    * "correct"
    
    * reviewing
    
    * rrr
    
    * 44444
    
    * addd
    
    * changes
    
    * reschange
    
    * clean code
    
    * adding
    
    * test
    
    * retest
    
    * restestt
    
    * change force
    
    * adding force
    
    * tee
    
    * packerading
    
    * modifying packervalidate
    
    * editing format
    
    * formating packer
    
    * packeraddingv4
    
    * build testing

--- config/database.js
+++ config/database.js
@@ -1,23 +1,23 @@
 import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
-import { fileURLToPath } from 'url';
-import path from 'path';
+// import { fileURLToPath } from 'url';
+// import path from 'path';
 
 dotenv.config();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 
-const mypath = path.join(__dirname, '../.env');
+
+
 const sequelize = new Sequelize(
     
-    process.env.DB_NAME, 
-  process.env.DB_USER, 
-    process.env.DB_PASSWORD, 
-    {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false 
+  process.env.DB_NAME, 
+process.env.DB_USER, 
+  process.env.DB_PASSWORD, 
+  {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  logging: false 
 });
-//
+
 export default sequelize;
+
--- middlewares/basicAuth.js
+++ middlewares/basicAuth.js
@@ -1,5 +1,4 @@
 
-
 import basicAuth from 'basic-auth';
 import bcrypt from 'bcrypt';
 
