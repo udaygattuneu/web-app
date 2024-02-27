75a08fb0dee9f91a6cc1b7283bc58fbf90e25d89
udaygattuneu <145166875+udaygattuneu@users.noreply.github.com>
2024-02-26 19:46:27 -0500

    test

--- config/database.js
+++ config/database.js
@@ -7,13 +7,13 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Now you can safely use __dirname to construct paths
+
 const mypath = path.join(__dirname, '../.env');
 const sequelize = new Sequelize(
     
     process.env.DB_NAME, 
-  process.env.DB_USR, 
-    process.env.DB_PWD, 
+  process.env.DB_USER, 
+    process.env.DB_PASSWORD, 
     {
     host: process.env.DB_HOST,
     dialect: 'mysql',
--- middlewares/basicAuth.js
+++ middlewares/basicAuth.js
@@ -1,27 +1,4 @@
 
-// import basicAuth from 'basic-auth';
-// import bcrypt from 'bcrypt';
-
-// const authenticate = async (req, user) => {
-//   const credentials = basicAuth(req); 
-//   console.log(credentials);
-//   const isValid = await isValidCredentials(credentials, user);
-//   return isValid; 
-// };
-
-// const isValidCredentials = async (credentials, user) => {
-//   console.log(user);
-  
-//   if (credentials && user) {
-//     const match = await bcrypt.compare(credentials.pass, user.password);
-//     return credentials.name === user.username && match;
-//   }
-//   return false;
-// };
-
-
-// export default authenticate;
-
 import basicAuth from 'basic-auth';
 import bcrypt from 'bcrypt';
 
